4c63021b62d431545d1868f28aa913e67679f980
Kishan <kp661@scarletmail.rutgers.edu>
2019-03-22 02:12:01 -0400

    basic recipe info

--- src/app/food-recipes/food-recipes.component.ts
+++ src/app/food-recipes/food-recipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodRecipesService } from '../food-recipes.service';
+import { FoodRecipes } from '../food-recipes';
 
 @Component({
   selector: 'app-food-recipes',
@@ -21,11 +22,20 @@ export class FoodRecipesComponent implements OnInit {
     this.foodRecipesService.getFoodRecipes(mealQuery).subscribe(
       obj => {
       const str = JSON.stringify(obj);
-      const output = [];
+      const output: FoodRecipes[] = [];
       str.substring(str.indexOf('\"recipe\"') + 12, str.length - 1).split('\"recipe\"').forEach(function (recipe) {
-        // console.log(recipe);
-        // console.log(recipe.substring(recipe.indexOf('\"image\"') + 11, recipe.indexOf('\"', recipe.indexOf('\"image\"') + 11)));
-        output.push(recipe.substring(recipe.indexOf('\"image\"') + 9, recipe.indexOf('\"', recipe.indexOf('\"image\"') + 9)));
+
+        const fR = {} as FoodRecipes;
+
+        fR.imageURL = recipe.substring(recipe.indexOf('\"image\"') + 9, recipe.indexOf('\"', recipe.indexOf('\"image\"') + 9));
+        fR.articleURL = recipe.substring(recipe.indexOf('\"url\"') + 7, recipe.indexOf('\"', recipe.indexOf('\"url\"') + 7));
+        fR.dietLabels = recipe.substring(recipe.indexOf('\"dietLabels\"') + 14, recipe.indexOf('\],',
+        recipe.indexOf('\"dietLabels\"') + 14)).split('\", \"');
+        fR.healthLabels = recipe.substring(recipe.indexOf('\"healthLabels\"') + 16, recipe.indexOf('\],',
+        recipe.indexOf('\"healthLabels\"') + 16)).split('\", \"');
+        // console.log(fR.dietLabels);
+
+        output.push(fR);
       });
       this.searchResults = output;
     });
