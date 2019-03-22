dd9a0055ae9633e8be13a0075e261d8e8d789046
Kishan <kp661@scarletmail.rutgers.edu>
2019-03-22 00:13:58 -0400

    http response for recipes

--- src/app/food-recipes/food-recipes.component.ts
+++ src/app/food-recipes/food-recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodRecipesService } from '../food-recipes.service';
 
 @Component({
   selector: 'app-food-recipes',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodRecipesComponent implements OnInit {
 
-  constructor() { }
+  searchResults: any;
+
+  constructor(
+    private foodRecipesService: FoodRecipesService
+  ) { }
 
   ngOnInit() {
   }
 
+  recipesQuery(mealQuery: string) {
+    this.foodRecipesService.getFoodRecipes(mealQuery).subscribe(
+      obj => {
+      const str = JSON.stringify(obj);
+      const output = [];
+      str.substring(str.indexOf('\"recipe\"') + 12, str.length - 1).split('\"recipe\"').forEach(function (recipe) {
+        // console.log(recipe);
+        // console.log(recipe.substring(recipe.indexOf('\"image\"') + 11, recipe.indexOf('\"', recipe.indexOf('\"image\"') + 11)));
+        output.push(recipe.substring(recipe.indexOf('\"image\"') + 9, recipe.indexOf('\"', recipe.indexOf('\"image\"') + 9)));
+      });
+      this.searchResults = output;
+    });
+  }
 }
