5b2e38c9e698ab158d6c025edefa7d7bb2b25b52
nikkim97 <niharikamishr@gmail.com>
2019-03-22 21:39:46 -0400

    pulling from master

--- src/app/food-recipes.ts
+++ src/app/food-recipes.ts
@@ -2,15 +2,9 @@ export interface FoodRecipes {
     recipeName: string;
     imageURL: string;
     articleURL: string;
-<<<<<<< HEAD
-    dietLabels: string[];
-    healthLabels: string[];
-}
-=======
     dietLabels: string;
     healthLabels: string;
     calories: string;
     servings: string;
     totalDaily: string;
 }
->>>>>>> 464a7a1703dea4f1c5df772c36afa3c13ec3e787
