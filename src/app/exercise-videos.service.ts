3936c3ba233f1209f4ae8eea963d9be513d776cd
nikkim97 <niharikamishr@gmail.com>
2019-03-23 12:00:30 -0400

    fixes

--- src/app/exercise-videos.service.ts
+++ src/app/exercise-videos.service.ts
@@ -14,7 +14,6 @@ export class ExerciseVideosService {
 
   getExerciseVideo(input: string) {
     const _url = this._url + '?' + 'app_id=' + this.app_id + '&app_key=' + this.app_key + '&q=' + input.split(' ').join(', ') + '&to=25';
-    // console.log(encodeURI(this._url));
     return this.http.get(encodeURI(_url));
   }
 }
