cb26a6809a04a601db2e9c84d01623139a31f593
nikkim97 <niharikamishr@gmail.com>
2019-03-22 21:22:54 -0400

    trailrunexercise

--- src/app/exercise-videos/exercise-videos.component.ts
+++ src/app/exercise-videos/exercise-videos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ExerciseVideosService } from '../exercise-videos.service';
+import { ExerciseVideos } from '../exercise-videos';
+
 
 @Component({
   selector: 'app-exercise-videos',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExerciseVideosComponent implements OnInit {
 
-  constructor() { }
+  searchResults: any;
+
+  constructor(
+    private exerciseVideosService: ExerciseVideosService
+  ) { }
 
   ngOnInit() {
+  }
+
+  exerciseQuery(videoQuery: string) {
+    this.exerciseVideosService.getExerciseVideo(videoQuery).subscribe(
+      obj => {
+      const str = JSON.stringify(obj);
+      const output: ExerciseVideos[] = [];
+      str.substring(str.indexOf('\"Video\"') + 12, str.length - 1).split('\"Video\"').forEach(function (video) {
+
+        const eR = {} as ExerciseVideos;
+
+        eR.imageURL = video.substring(video.indexOf('\"image\"') + 9, video.indexOf('\"', video.indexOf('\"image\"') + 9));
+        eR.articleURL = video.substring(video.indexOf('\"url\"') + 7, video.indexOf('\"', video.indexOf('\"url\"') + 7));
+        // console.log(eR.dietLabels);
+
+        output.push(eR);
+      });
+      this.searchResults = output;
+    });
   }
 
 }
