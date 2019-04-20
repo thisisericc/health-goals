653a2166ac5d192f892809aa34a9f0dc24213ca7
Pranathi Singanamala <pranathi@nbp-228-195.nbp.ruw.rutgers.edu>
2019-04-20 16:43:39 -0400

    update commit changes

--- server/HealthGoals/__init__.py
+++ server/HealthGoals/__init__.py
@@ -230,3 +230,30 @@ def sign_up(FirstName,LastName, Email, Password, Description, Goals, DietaryRest
         return make_response(str(e), 400)
     except Exception as e:
         return make_response(str(e), 500)
+
+@app.route('/api/groups', methods=["GET"])
+def get_GroupInfo(): 
+    return jsonify(database.get_GroupInfo())
+
+@app.route('/api/group/<groupname>', methods=["GET"])
+def getGroup(groupname): 
+    return jsonify(database.getGroup(groupname))
+
+@app.route('/api/searchForGroups/<name>', methods=["GET"])
+def searchForGroups(name): 
+    return jsonify(database.searchForGroups(name))
+
+@app.route('/api/filterexercise/<exercise>', methods=["GET"])
+def filterexercise(exercise): 
+    return jsonify(database.filterexercise(exercise))
+
+@app.route('/api/filtercalories/<calories>', methods=["GET"])
+def filtercalories(calories): 
+    return jsonify(database.filtercalories(calories))
+@app.route('/api/members/<Name>', methods=["GET"])
+def getMemberInfo(Name): 
+    return jsonify(database.getMemberInfo(Name))
+
+@app.route('/api/joingroup/<groupname>/<username>', methods=["GET"])
+def JoinGroup(groupname, username): 
+    return jsonify(database.JoinGroup(groupname, username))
