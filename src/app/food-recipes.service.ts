e5231f9ae8761420195e61f98f687cb64670e0b4
Arti Patel <Art@Artis-MBP.home>
2019-04-23 03:22:29 -0400

    buggg

--- src/app/food-recipes.service.ts
+++ src/app/food-recipes.service.ts
@@ -133,6 +133,10 @@ export class FoodRecipesService {
     return this.http.get('/api/insert_saved_recipes/'+ID+'/'+Name+'/'+URL)
   }
 
+  insert(ID: any, Name: any){
+    return this.http.get('/api/insert/'+ID+'/'+Name)
+  }
+
   private errorHandler(error: HttpErrorResponse) {
     return throwError(
       error.error
